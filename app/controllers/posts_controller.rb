5c816e53f94c1e64b7eacbb3ca4c21ef730504fb
miomio-miomio <rainingpath8@gmail.com>
2023-09-12 21:06:16 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,15 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    # binding.pry
+    # Post.create(content: params[:content])
+    # 新たに投稿されたメモの内容を変数postに格納
+    post = Post.create(content: params[:content])
+
+    # 同期処理
+    # redirect_to action: :index
+    
+    # 非同期処理
+    render json:{ post: post }
   end
 end
